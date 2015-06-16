678dedc1aef0a02411131e1bc01af27352e4a9ac
Kopachris <kopachris@gmail.com>
2015-06-16 13:04:21 -0600

    Created directory tree, placeholder files

---
+++ seshet/__init__.py
@@ -0,0 +1,9 @@
+"""Seshet bot package. Initializes configuration and defines bot classes
+and exceptions.
+"""
+
+
+__author__ = "Christopher Koch"
+__copyright__ = "Copyright (c) 2015 Christopher Koch"
+__license__ = "3-clause BSD"
+__version__ = "0.1.0"
