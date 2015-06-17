5220aa92ce549a10fed46e731602b63aafc6d151
Kopachris <kopachris@gmail.com>
2015-06-17 09:58:40 +0000

    Updated __all__

--- seshet/__init__.py
+++ seshet/__init__.py
@@ -7,3 +7,5 @@ __author__ = "Christopher Koch <ch_koch@outlook.com>"
 __copyright__ = "Copyright (c) 2015 Christopher Koch"
 __license__ = "3-clause BSD"
 __version__ = "0.1.0"
+
+__all__ = ['bot', 'utils']
