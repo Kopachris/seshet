ac4b4f839c4fc902fe197004275dff68557cc1d0
Kopachris <kopachris@gmail.com>
2015-06-20 10:32:17 +0000

    Copied Storage code from gluon.storage (see copyright in docstring)

--- seshet/utils.py
+++ seshet/utils.py
@@ -1,4 +1,119 @@
-"""Various utility classes used by the bot and command modules."""
+"""Various utility classes used by the bot and command modules.
+
+`Storage` is copied from gluon.storage, part of the web2py framework,
+    Copyrighted by Massimo Di Pierro <mdipierro@cs.depaul.edu>,
+    License: LGPLv3 (http://www.gnu.org/licenses/lgpl.html)
+"""
+
+class Storage(dict):
+    """A Storage object is like a dictionary except `obj.foo` can be used
+    in addition to `obj['foo']`, and setting obj.foo = None deletes item foo.
+
+    Example:
+
+        >>> o = Storage(a=1)
+        >>> print o.a
+        1
+
+        >>> o['a']
+        1
+
+        >>> o.a = 2
+        >>> print o['a']
+        2
+
+        >>> del o.a
+        >>> print o.a
+        None
+
+    """
+    
+    __slots__ = ()
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+    __getitem__ = dict.get
+    __getattr__ = dict.get
+    __getnewargs__ = lambda self: getattr(dict,self).__getnewargs__(self)
+    __repr__ = lambda self: '<Storage %s>' % dict.__repr__(self)
+    __getstate__ = lambda self: None
+    __copy__ = lambda self: Storage(self)
+
+    def getlist(self, key):
+        """Returns a Storage value as a list.
+
+        If the value is a list it will be returned as-is.
+        If object is None, an empty list will be returned.
+        Otherwise, `[value]` will be returned.
+
+        Example output for a query string of `?x=abc&y=abc&y=def`::
+
+            >>> request = Storage()
+            >>> request.vars = Storage()
+            >>> request.vars.x = 'abc'
+            >>> request.vars.y = ['abc', 'def']
+            >>> request.vars.getlist('x')
+            ['abc']
+            >>> request.vars.getlist('y')
+            ['abc', 'def']
+            >>> request.vars.getlist('z')
+            []
+
+        """
+        
+        value = self.get(key, [])
+        if value is None or isinstance(value, (list, tuple)):
+            return value
+        else:
+            return [value]
+
+    def getfirst(self, key, default=None):
+        """Returns the first value of a list or the value itself when given a
+        `request.vars` style key.
+
+        If the value is a list, its first item will be returned;
+        otherwise, the value will be returned as-is.
+
+        Example output for a query string of `?x=abc&y=abc&y=def`::
+
+            >>> request = Storage()
+            >>> request.vars = Storage()
+            >>> request.vars.x = 'abc'
+            >>> request.vars.y = ['abc', 'def']
+            >>> request.vars.getfirst('x')
+            'abc'
+            >>> request.vars.getfirst('y')
+            'abc'
+            >>> request.vars.getfirst('z')
+
+        """
+        
+        values = self.getlist(key)
+        return values[0] if values else default
+
+    def getlast(self, key, default=None):
+        """Returns the last value of a list or value itself when given a
+        `request.vars` style key.
+
+        If the value is a list, the last item will be returned;
+        otherwise, the value will be returned as-is.
+
+        Simulated output with a query string of `?x=abc&y=abc&y=def`::
+
+            >>> request = Storage()
+            >>> request.vars = Storage()
+            >>> request.vars.x = 'abc'
+            >>> request.vars.y = ['abc', 'def']
+            >>> request.vars.getlast('x')
+            'abc'
+            >>> request.vars.getlast('y')
+            'def'
+            >>> request.vars.getlast('z')
+
+        """
+        
+        values = self.getlist(key)
+        return values[-1] if values else default
+
 
 class KVStore:
     """Create a key/value store in the bot's database for each
