421fb70257dd31732c58f1f09bc74e199d4f5f8a
Kopachris <kopachris@gmail.com>
2015-07-07 06:47:59 +0000

    Cleaned up package and added .gitignore

--- test.py
+++ test.py
@@ -4,20 +4,29 @@ from seshet.bot import SeshetBot
 from pydal import DAL, Field
 from datetime import datetime
 
-db = DAL('sqlite://seshet.db')
-db.define_table('event_log',
-                Field('event_time', 'datetime', default=datetime.today()),
-                Field('event_type', 'string'),
-                Field('source', 'string'),
-                Field('target', 'string'),
-                Field('message', 'string'),
-                Field('host', 'string'),
-                Field('parms', 'string'),
-                )
+def main():
+    print("Opening database...")
+    db = DAL('sqlite://seshet.db')
+    print("Defining database table...")
+    db.define_table('event_log',
+                    Field('event_time', 'datetime', default=datetime.today()),
+                    Field('event_type', 'string'),
+                    Field('source', 'string'),
+                    Field('target', 'string'),
+                    Field('message', 'string'),
+                    Field('host', 'string'),
+                    Field('parms', 'string'),
+                    )
+    
+    print("\tDatabase OK.\nInitializing bot...")
+    bot = SeshetBot('SeshetBot', db=db)
+    bot.user = 'SeshetBot'
+    bot.real_name = 'SeshetBot'
+    
+    print("Connecting...", end=' ')
+    bot.connect('irc.esper.net', channel='#hard-light')
+    print("Connected.\nStarting main loop...")
+    bot.start()
 
-bot = SeshetBot('SeshetBot', db=db)
-bot.user = 'SeshetBot'
-bot.real_name = 'SeshetBot'
-
-bot.connect('irc.esper.net', channel='#hard-light')
-bot.start()
+if __name__ == '__main__':
+    main()
