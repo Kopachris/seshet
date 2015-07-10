d02ca26e02dd1deb5114478023e2d44499e70212
Christopher Koch <chris@ns504287.ip-142-4-209.net>
2015-07-10 13:29:05 +0000

    stuff

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from seshet import bot
 
 nick = input("Nickname? (Seshet) ") or "Seshet"
 server = input("Server? (chat.freenode.net) ") or "chat.freenode.net"
-port = int(input("Port? (6667) ")) or 6667
+port = input("Port? (6667) ") or 6667
 channel = input("Channel to join? (#botwar) ") or "#botwar"
 
 seshetbot = bot.SeshetBot(nick)
 
 print("Connecting...", end=" ")
-seshetbot.connect(server, port=port, channel=channel)
+seshetbot.connect(server, port=int(port), channel=channel)
 print("Done.\nPress Ctrl-C to quit.")
 seshetbot.start()
