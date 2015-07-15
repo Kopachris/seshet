e718e6a42dbf29a115c66805d0690e15a4d66d40
Kopachris <kopachris@gmail.com>
2015-07-15 10:20:30 +0000

    outiled config.py

--- seshet/config.py
+++ seshet/config.py
@@ -2,3 +2,128 @@
 configuration to SeshetBot instance.
 """
 
+from configparser import ConfigParser
+
+from pydal import DAL, Field
+
+
+default_config = """
+[connection]
+# passed to SeshetBot.connect()
+server: chat.freenode.net
+port: 6667
+channels: #botwar
+ssl: False
+
+[client]
+nickname: Seshet
+user: seshet
+realname: seshetbot
+
+[welcome]
+# stuff sent by the bot after connecting
+use_nickserv: False
+nickserv_pass:
+user_mode: -x
+
+[locale]
+timezone: UTC
+locale: en_US
+# see docs for datetime.strftime
+date_fmt: %m%d%y
+# 071415
+time_fmt: %H:%M:%S
+# 11:49:57
+short_datetime_fmt: %Y-%m-%d %H:%M:%S
+# 2015-07-14 11:49:57
+long_datetime_fmt: %A %d %B %Y at %H:%M:%S %Z
+# Tuesday 14 July 2015 at 11:49:57 UTC
+
+[database]
+use_db: True
+db_string: sqlite://seshet.db
+
+[logging]
+# if using db, this will be ignored
+file: logs/%(target)s_%(date)s.log
+
+[debug]
+# corresponds to levels in logging module or None
+verbosity: warning
+file: seshet-debug.log
+"""
+
+
+testing_config = """
+[connection]
+# passed to SeshetBot.connect()
+server: chat.freenode.net
+port: 6667
+channels: #botwar
+ssl: False
+
+[client]
+nickname: Seshet
+user: seshet
+realname: seshetbot
+
+[welcome]
+# stuff sent by the bot after connecting
+use_nickserv: False
+nickserv_pass:
+user_mode: -x
+
+[locale]
+timezone: UTC
+locale: en_US
+# see docs for datetime.strftime
+date_fmt: %m%d%y
+# 071415
+time_fmt: %H:%M:%S
+# 11:49:57
+short_datetime_fmt: %Y-%m-%d %H:%M:%S
+# 2015-07-14 11:49:57
+long_datetime_fmt: %A %d %B %Y at %H:%M:%S %Z
+# Tuesday 14 July 2015 at 11:49:57 UTC
+
+[database]
+# no db connection for testing
+use_db: False
+
+[logging]
+# if using db, this will be ignored
+file: logs/%(target)s_%(date)s.log
+
+[debug]
+# corresponds to levels in logging module or None
+verbosity: debug
+file: seshet-debug.log
+"""
+
+
+def build_db_tables(db):
+    """Build Seshet's basic database schema. Requires one parameter,
+    `db` as `pydal.DAL` instance.
+    """
+    if not isinstance(db, DAL) or not db._uri:
+        raise Exception("Need valid DAL object to define tables")
+        
+
+def apply_config(bot, config=None, config_file=None):
+    """Take either a parsed configuration or a config file and apply it
+    to a SeshetBot instance. Return a dict containing server, port, and
+    default channels to pass as arguments to `SeshetBot.connect()`.
+    
+    Required argument:
+        bot - must be SeshetBot instance
+    
+    Optional arguments:
+        config - ConfigParser, dict, or str instance
+        config_file - valid file path
+        
+        If config is present, it will take precedence over config_file.
+        If using config_file, will attempt to read the configuration from
+        the specified file using ConfigParser. If neither are used, will
+        apply default configuration defined in this module.
+    """
+    pass
