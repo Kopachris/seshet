42705ecc9dbe51807fda0da4e0e11340c2c05699
Kopachris <kopachris@gmail.com>
2015-07-16 04:54:47 -0600

    Added setup.py

--- seshet/__init__.py
+++ seshet/__init__.py
@@ -2,10 +2,4 @@
 and exceptions.
 """
 
-
-__author__ = "Christopher Koch <ch_koch@outlook.com>"
-__copyright__ = "Copyright (c) 2015 Christopher Koch"
-__license__ = "3-clause BSD"
-__version__ = "0.1.0"
-
 __all__ = ['bot', 'utils']
---
+++ setup.py
@@ -0,0 +1,39 @@
+"""Seshet - Modular, dynamic IRC bot"""
+
+from setuptools import setup
+
+
+long_description = open(
+    path.join(
+        path.dirname(__file__),
+        'README.md'
+    )
+).read()
+
+setup(
+    name='seshet',
+    version='0.1.0',
+    url='https://github.com/kopachris/seshet',
+    license='BSD',
+    author='Christopher Koch',
+    author_email='ch_koch@outlook.com',
+    description='Modular, dynamic IRC bot',
+    long_description='',  # update later
+    packages=['seshet'],
+    install_requires=[
+        'ircutils3',
+        'pydal',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+    ]
+)
